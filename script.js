4e64b4e74ab363d35139892cf2d35d74256b5f49
Jerico Dcosta <68363466+Jerico007@users.noreply.github.com>
2023-08-25 12:54:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function resolvePromise(){
+	let prom = new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000);	
+	}).then((data)=>{
+		document.getElementById("output").innerHTML = data;
+	});
+}
+
+
+resolvePromise();
